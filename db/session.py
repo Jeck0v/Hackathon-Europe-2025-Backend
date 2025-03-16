a5049734f59f9d54a4333a3751d912a1c5adeafa
Jeck0v <arnaud.fischer75@gmail.com>
2025-03-16 23:50:58 +0100

    :ambulance: Fix DB 2 ?

--- db/session.py
+++ db/session.py
@@ -4,7 +4,8 @@ client = MongoClient(
     host="mongo",
     port=27017,
     username="article11",
-    password="-)i8N~x4r9cVX8"
+    password="1J7A4s0omlrjRM03TfE0",
+    authSource="admin"
 )
 
 db = client.article11_db
--- middlewares/security.py
+++ middlewares/security.py
@@ -7,5 +7,11 @@ logger = logging.getLogger("uvicorn")
 class SecurityMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         logger.info(f"Incoming request: {request.method} {request.url}")
-        response = await call_next(request)
+        logger.debug(f"Request headers: {request.headers}")
+        try:
+            response = await call_next(request)
+        except Exception as e:
+            logger.error(f"Error occurred while processing the request: {str(e)}")
+            raise e
+
         return response
