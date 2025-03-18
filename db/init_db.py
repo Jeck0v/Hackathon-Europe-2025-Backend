50bf5c5eac8bbf6f4ff0d5252a8ffd8df78bf238
Kae134 <louisdondey@gmail.com>
2025-03-18 17:26:09 +0100

    🧱 Added models/schemas

--- db/init_db.py
+++ db/init_db.py
@@ -1,8 +1,48 @@
 from db.session import get_db
 
+data_user = [
+    {
+        "id" : "test",
+        "username":"Jecko",
+        "name" : "Doe",
+        "firstname" : "John",
+        "age" : "21",
+        "country" : "France",
+        "gender" : "other",
+        "email": "johndoe@gmail.com",
+        "password" : "1234321", 
+        "consent" : False,
+        "identity_verif" : False,
+        "historic" : [],
+        "streak" : 0
+    }
+]
+
+data_feed = [
+    {
+        "id_subject" : "1",
+        "short_description" : "",
+        "image" : "",
+        "context" : "",
+        "impact" : [],
+        "source" : "",
+        "votes" : {"0" : 0, "1" : 0, "2" : 0}
+    }
+]
+
+data_compromise = [
+    {
+        "id_subject" : "1",
+        "id_user" : "1",
+        "text" : ""
+    }
+]
+
+
+
 def initialize_database():
     db = get_db()
-    required_collections = ['users', 'votes', 'news', 'social']
+    required_collections = ['users', 'feed', 'compromise']
     existing_collections = db.list_collection_names()
     for collection in required_collections:
         if collection not in existing_collections:
--- schemas/user.py
+++ schemas/user.py
@@ -6,29 +6,51 @@ class UserResponse(BaseModel):
     username: str
     firstname: str
     name: str
+    age: int
+    gender: str
+    country: str
     email: EmailStr
-    consent: bool
+    password: str
+    consent: Optional[bool] = False
+    identity_verif: Optional[bool] = False
     role: Optional[str] = "user"
+    historic: list
+    streak: int
 
     class Config:
         from_attributes = True
 
 class UserCreate(BaseModel):
+    id: str
     username: str
     firstname: str
     name: str
+    age: int
+    gender: str
+    country: str
     email: EmailStr
     password: str
     consent: Optional[bool] = False
+    identity_verif: Optional[bool] = False
     role: Optional[str] = "user"
+    historic: list
+    streak: int
 
 class UserUpdate(BaseModel):
+    id: Optional[str] = None 
     username: Optional[str] = None
     firstname: Optional[str] = None
     name: Optional[str] = None
+    age: Optional[int] = None
+    gender: Optional[str] = None
+    country: Optional[str] = None
     email: Optional[EmailStr] = None
     password: Optional[str] = None
-    consent: Optional[bool] = None
+    consent: Optional[bool] = False
+    identity_verif: Optional[bool] = False
+    role: Optional[str] = "user"
+    historic: Optional[list] = None
+    streak: Optional[int] = None
 
 class Token(BaseModel):
     access_token: str
