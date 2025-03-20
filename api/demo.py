b967ca59c168bf1feee57ca896fbb33e4026ce3b
Jeck0v <arnaud.fischer75@gmail.com>
2025-03-20 22:04:07 +0100

    :beers: fix demo/subject_id

--- api/demo.py
+++ api/demo.py
@@ -19,15 +19,16 @@ async def get_all_demo_compromises():
 
 @router.get("/demo/{subject_id}", response_model=DEMOCompromiseResponse)
 async def get_demo_compromise_by_id(subject_id: str):
-    demo_compromise = db.DEMO_compromise.find_one({"_id": ObjectId(subject_id)})
+    demo_compromise = db.DEMO_compromise.find_one({"_id": subject_id})
     if not demo_compromise:
         raise HTTPException(status_code=404, detail="Data compromise not found.")
 
-    return DEMOCompromiseResponse(
-        id_subject=str(demo_compromise["_id"]),
-        text=demo_compromise["text"]
-    )
-
+    return [
+        DEMOCompromiseResponse(
+        id_subject=str(data["_id"]),
+        text=data["text"]
+        ) for data in demo_compromise
+    ]
 
 @router.post("/demo", response_model=DEMOCompromiseResponse)
 async def create_demo_compromise(DEMO_compromise: DEMOCompromiseCreate):
